10beeabaf9e3b99cca34fef2a0c99621aaf49946
Saitejaa5 <saitejaa.ana5@gmail.com>
2025-01-09 13:22:23 +0530

    Add files via upload

---
+++ my_module.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+greet = lambda name : print(f"Good Morning {name}!")
+
+
+# In[ ]:
+
+
+# greet ("Santhosh")
+
+
+# In[5]:
+
+
+product = lambda a,b,c : a*b*c 
+
+
+# In[ ]:
+
+
+ # product(20,30,50)
+
+
+# In[ ]:
+
+
+even = lambda L : [x for x in L if x%2 == 0]
+
+
+# In[ ]:
+
+
+# my_list = [100,5,6,78,7,1,1,9,0,3,2,10,]
+# even(my_list)
+
